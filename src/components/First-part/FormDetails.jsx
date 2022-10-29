1e37793f1385eae8eb3cc8d955292a409c6e07e6
unknown <mohmmedtouhid007@gmail.com>
2022-10-29 19:25:37 +0600

    Added One field

--- src/components/First-part/FormDetails.jsx
+++ src/components/First-part/FormDetails.jsx
@@ -3,7 +3,7 @@ import "./FormDetails.css";
 const FormDetails = () => {
   return (
     <div className="first-part">
-      <h1>Learn to code by watching others</h1>
+      <h1>Learn to code by watching others -[TOUHID]</h1>
       <p>
         See how experienced developers solve problems in real-time. Watching
         scripted tutorials is great, but understanding how developers think is
